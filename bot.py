196676d8945a025e05503c8e6ec0a043e5706fa8
kalupakaya <141867772+kalupakaya@users.noreply.github.com>
2023-08-12 23:12:52 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+# Replace with your API ID and API hash
+API_ID = 4730697
+API_HASH = '5b451ec5950aae4b8e914359c7975dca'
+BOT_TOKEN = '6467875273:AAHxShcMmP1NEJTQI9MakV9LmGUhlK7O6y0'
+
+# Create a Pyrogram client instance
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+# Define a command handler for the /start command
+@app.on_message(filters.command("start"))
+def start_command_handler(client: Client, message: Message):
+    # Reply to the user with a welcome message
+    message.reply_text("Welcome to MyBot! Type /help for more options.")
+
+# Run the bot
+if __name__ == "__main__":
+    app.run()
